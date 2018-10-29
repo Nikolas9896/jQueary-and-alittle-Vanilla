f698c033946876a1ded2b82464ee381c56143137
Nikolas9896 <nhlynka@gmail.com>
2018-10-29 17:21:27 +0200

    add div.highlight in jQueary

--- jQuery1.js
+++ jQuery1.js
@@ -8,6 +8,6 @@ var Mystyle = {
 $("h1").css(Mystyle); // SelectAll <h1>
 
 $("div").css("background", "purple");
-$(".highlight").css("width", "200px");
+$("div.highlight").css("width", "200px");
 $("#third").css("border", "2px solid orange");
 $("div").first().css("color", "pink");
