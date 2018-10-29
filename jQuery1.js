c1c909b87558ffcf02e491c0d27a20d58de457e4
Nikolas9896 <nhlynka@gmail.com>
2018-10-29 13:40:47 +0200

    Add styles and made exercise

--- jQuery1.js
+++ jQuery1.js
@@ -1,5 +1,13 @@
 //****************** First file **************
 
-$("a"); // SelectAll <a>
-$("#adorable"); 
-$(".litte"); 
+var Mystyle = {
+	color: "red",
+	background: "pink",
+	border: "2px solid blue"
+}
+$("h1").css(Mystyle); // SelectAll <h1>
+
+$("div").css("background", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "2px solid orange");
+$("div").first().css("color", "pink");
