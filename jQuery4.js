b2e4d53544829b64c4bea31f792101380b510d36
Nikolas9896 <nhlynka@gmail.com>
2018-10-30 13:39:15 +0200

    Check the slideToggle

--- jQuery4.js
+++ jQuery4.js
@@ -1,5 +1,11 @@
+// $("button").on("click",function(){
+// $('div').fadeToggle(1000, function(){
+// 	alert("Fade out finished");
+// });
+// });
+
 $("button").on("click",function(){
-$('div').fadeToggle(1000, function(){
+$('div').slideToggle(1000, function(){
 	alert("Fade out finished");
 });
 });
