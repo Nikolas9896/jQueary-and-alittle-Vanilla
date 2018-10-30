f57c2e2a312d4d478ba60e3f1d04aeddb3b94300
Nikolas9896 <nhlynka@gmail.com>
2018-10-30 10:07:14 +0200

    add default to selected

--- jQuery2.js
+++ jQuery2.js
@@ -1,6 +1,6 @@
 //$("h1").css("color", "steelblue");
 $('ul').text();
-$('ul').html("<li> I hacked your UL!</li><li>Rusty is still adorable!</li>");
+//$('ul').html("<li> I hacked your UL!</li><li>Rusty is still adorable!</li>");
 $('img').css("width", "200px");
 $('img:nth-of-type(2)').attr("src", "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprodmigration%2Fweb%2Fbin%2F9549760f-90b2-3602-af1b-2161836fb1b1.jpg?crop=779%2C519%2C0%2C0&resize=685");
 $('input').attr("type", "color");
@@ -12,5 +12,8 @@ console.log($('select').val());
 
 $('h1').addClass('correct');
 $('li').addClass('wrong');
+$('li').removeClass('wrong').addClass('correct');
+$('h1').toggle('correct');
+$('h1').toggle('correct');
 	
 
